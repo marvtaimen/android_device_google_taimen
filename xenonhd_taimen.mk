41a416f35f453dec68900ea6165cd4b2fec73915
REV3NT3CH <rev3nt3ch@gmail.com>
2019-05-05 17:19:52 -0700

    XenonHD-ify

---
+++ xenonhd_taimen.mk
@@ -0,0 +1,38 @@
+# Copyright (C) 2019 The XenonHD Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common XenonHD stuff.
+$(call inherit-product, vendor/xenonhd/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/taimen/aosp_taimen.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_NAME := xenonhd_taimen
+PRODUCT_DEVICE := taimen
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 2 XL
+TARGET_MANUFACTURER := LGE
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=taimen \
+    BUILD_FINGERPRINT=google/taimen/taimen:9/PQ3A.190605.003/5524043:user/release-keys \
+    PRIVATE_BUILD_DESC="taimen-user 9 PQ3A.190605.003 5524043 release-keys"
+
+$(call inherit-product-if-exists, vendor/google/taimen/taimen-vendor.mk)
